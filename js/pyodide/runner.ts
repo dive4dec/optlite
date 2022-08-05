7255c4943a7088e7767a90bda15824f105508c29
ccha23 <42616371+ccha23@users.noreply.github.com>
2022-08-05 23:46:58 +0800

    fix duplicate reply from worker

--- js/pyodide/runner.ts
+++ js/pyodide/runner.ts
@@ -1,11 +1,12 @@
 import { OptLite } from './global'
 
 const pyodideWorker = new Worker("worker.js");
-
 const callbacks = {};
 
+// ask worker to initialize pyodide based on the configuration 
+// in a global OptLite object predefined before loading pyodide.
 const initWorker = (() => {
-  let id = -1;
+  let id = -1; // use -ve job id for initialization
   return () => {
     return new Promise((onSuccess) => {
       callbacks[id] = onSuccess;
@@ -18,21 +19,24 @@ const initWorker = (() => {
 })();
 let init = initWorker();
 
+// handle results from worker
 pyodideWorker.onmessage = async (event) => {
   const { id, ...data } = event.data;
   const onSuccess = callbacks[id];
-  delete callbacks[id];
-  onSuccess(data); 
+  if (onSuccess) { 
+    // Only handle the first reply from worker for each job
+    delete callbacks[id];
+    onSuccess(data); 
+  }
 };
 
 const asyncRun = (() => {
-  let id = 0; // identify a Promise
-  return (script,context) => {
-    // the id could be generated more carefully
+  let id = 0; // identify each visualization by a non-negative id
+  return (script: string, context: any) => {
     id = (id + 1) % Number.MAX_SAFE_INTEGER;
     return new Promise((onSuccess) => {
-      console.log(init)
       init.then(() => {
+        // visualize after initialize
         callbacks[id] = onSuccess;
         pyodideWorker.postMessage({
           ...context,
@@ -45,59 +49,3 @@ const asyncRun = (() => {
 })();
 
 export { asyncRun };
-/*
-export class PyodideRunner{
-    pyodide_py : any;
-    PyodideRunner(){
-        this.pyodide_py = null;
-    }
-    async getPyodide(){
-        if(this.pyodide_py === null) 
-            //this.pyodide_py = await import('../lib/pyodide'); // await import('pyodide/pyodide.js')
-            this.pyodide_py = await import('pyodide/load-pyodide.js');
-        return this.pyodide_py;
-    }
-    public async runCode(callback:any){
-        
-        let pyodide_py = await this.getPyodide();
-        let pyodide = await pyodide_py.loadPyodide();
-        let output = await pyodide.runPythonAsync(script);
-        callback(output);
-    }
-}
-let script : string = `
-import sys, pg_logger, json
-from optparse import OptionParser
-
-request = False
-try:
-	import StringIO # NB: don't use cStringIO since it doesn't support unicode!!!
-except:
-	import io as StringIO # py3
-		   
-def jsonp(request, dictionary):
-    if (request):
-        return "%s(%s)" % (request, dictionary)
-    return dictionary
-
-
-out_s = StringIO.StringIO()
-
-
-def json_finalizer(input_code, output_trace):
-	ret = dict(code=input_code, trace=output_trace)
-	json_output = json.dumps(ret, indent=None)
-	out_s.write(json_output)
-
-
-pg_logger.exec_script_str_local(code,
-							  None,
-							  False,
-							  False,
-							  json_finalizer)
-
-
-
-jsonp(False, out_s.getvalue())	  
-`;
-*/
